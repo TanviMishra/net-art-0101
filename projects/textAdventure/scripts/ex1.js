2da9fac9ff0c2c970216c8ed2407fe76b7997c01
TanviMishra <misht490@newschool.edu>
2022-09-14 18:08:06 -0400

    linked

--- projects/textAdventure/scripts/ex1.js
+++ projects/textAdventure/scripts/ex1.js
@@ -1,10 +1,127 @@
 body = document.querySelector("body");
 grid = document.querySelector("#grid");
+smallImgsDiv = document.querySelector("#smallImgs");
 rules = document.querySelector("#ruleButton");
 rulePopup = document.querySelector("#rulePopup");
 clickInfo = document.querySelector("#clickInfo");
 progressWrapper = document.querySelector("#progressWrapper");
 
+winText = document.querySelector("#winText");
+linkText = document.querySelector("#linkText");
+
+let textJson = {
+  hints: [
+    {
+      type: "hint",
+      srno: 0,
+      hover: "0.  ",
+      text: " is home to many-a tourist attractions. Upon arrival, you can start you tour with an icecream and seagull viewing party. Remember to keep a hold on your shoes so that the gulls don't grab at them...",
+    },
+    {
+      type: "hint",
+      srno: 1,
+      hover: "1.  ",
+      text: " is a place of deep worship, with a clamor of bells that can even set the gulls soaring. People come here with a voracious appetite for inner pace not understanding that their greed will drown them...",
+    },
+    {
+      type: "hint",
+      srno: 2,
+      hover: "2.  ",
+      text: " is a lake with unknown depths of sorrow. It weeps itself to sleep every night troubled as the world peacefully drifts on be. It's starved for some company and comfort...",
+    },
+    {
+      type: "hint",
+      srno: 3,
+      hover: "3.  ",
+      text: " is a place with nothing to boast of except its food. There is no greater fool than the person who arrives with a full stomach. Fill yourself up with four cheese pizzas and perogies...",
+    },
+    {
+      type: "hint",
+      srno: 4,
+      hover: "4.  ",
+      text: " is a mountain range populated by sluggish mountain cats awaiting their final days",
+    },
+  ],
+  alerts: [
+    {
+      type: "alert",
+      hover: " ",
+      text: " is a lake of unknown origin. it was first discovered by dacoits fleeing the nearby town and they fretfully took shelter along the banks... ",
+      alertText:
+        "this lake is only a rest stop for the uncooth. Are you uncooth!?",
+    },
+    {
+      type: "alert",
+      hover: " ",
+      text: " is a ghost town. It was abandoned half a century back due to unknown reasons. All they left behind is a papertrail following flying monkeys and the collapse of society..",
+      alertText: "only ghosts welcome here",
+    },
+    {
+      type: "alert",
+      hover: " ",
+      text: " is a glacial path. While the views are unmatched, it can be a treacherous journey. ",
+      alertText: "oops, watch your step",
+    },
+    {
+      type: "alert",
+      hover: " ",
+      text: " is the valley of vipers. It is mostly visited by herpetologist who study said vipers",
+      alertText: "excuse me, did you not hear me say VIPERS!",
+    },
+    {
+      type: "alert",
+      hover: " ",
+      text: " is a beach broadway, perfect for summer lounging.",
+      alertText: "the tides are rising, it's better to head back now",
+    },
+  ],
+  urls: [
+    {
+      type: "url",
+      hover: "0.  ",
+      text: " is a volcanic mountain that has been dormant for 400 years.",
+      urlText: "BOOM! Guess the mountain is no longer dormant",
+      urlLink: "./projects/textAdventure/ex1Link.html",
+    },
+    {
+      type: "url",
+      hover: "1.  ",
+      text: " is a city in the midst of a recession. The pharmaceutical company here just declared bankruptcy, so you know there's no saving the city",
+      urlText:
+        "it's already hard to find jobs, you don't need to add a challenge",
+      urlLink: "./projects/textAdventure/ex1Link.html",
+    },
+    {
+      type: "url",
+      hover: "2.  ",
+      text: " is a nucleur powerplant that provides employment to hundreds of workers from the town next door. That's why the government turns a blind eye to some of the lapsed building certifications",
+      urlText: "you need to learn about red flags!",
+      urlLink: "./projects/textAdventure/ex1Link.html",
+    },
+  ],
+  imgs: [
+    {
+      type: "img",
+      hover: "2.  ",
+      text: " is a picturesque viewing point",
+    },
+  ],
+};
+let tempCol = getComputedStyle(document.body);
+let colorArr = [
+  tempCol.getPropertyValue("--col1opt1"),
+  tempCol.getPropertyValue("--col1opt2"),
+  tempCol.getPropertyValue("--col2opt1"),
+  tempCol.getPropertyValue("--col2opt2"),
+  tempCol.getPropertyValue("--col2opt3"),
+];
+let placeArr = [];
+let positionArr = [];
+let photosArr = [];
+let hoverTextCount = 0;
+let correctClueNumber = -1;
+let gridSize = 2;
+const clueMax = 4; //the total number of places clicked
 let placeOptions = [
   "Garhkaru",
   "Khanlag",
@@ -57,150 +174,11 @@ let placeOptions = [
   "Rangura",
   "Lamsuaka",
 ];
-let hoverTextJson = {
-  hints: [
-    {
-      type: "hint",
-      srno: 0,
-      hover: "0. Only pain is intellectual",
-      text: "0. With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.",
-    },
-    {
-      type: "hint",
-      srno: 1,
-      hover: "1. Only pain is intellectual",
-      text: "1. With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.",
-    },
-    {
-      type: "hint",
-      srno: 2,
-      hover: "2. Only pain is intellectual",
-      text: "2. With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.",
-    },
-    {
-      type: "hint",
-      srno: 3,
-      hover: "3. Only pain is intellectual",
-      text: "3. With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.",
-    },
-    {
-      type: "hint",
-      srno: 4,
-      hover: "4. Only pain is intellectual",
-      text: "4. With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.",
-    },
-    {
-      type: "hint",
-      srno: 5,
-      hover: "5. Only pain is intellectual",
-      text: "5. With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.",
-    },
-    {
-      type: "hint",
-      srno: 6,
-      hover: "6. Only pain is intellectual",
-      text: "6. With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.",
-    },
-    {
-      type: "hint",
-      srno: 7,
-      hover: "7. Only pain is intellectual",
-      text: "7. With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.",
-    },
-    {
-      type: "hint",
-      srno: 8,
-      hover: "8. Only pain is intellectual",
-      text: "8. With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.",
-    },
-    {
-      type: "hint",
-      srno: 9,
-      hover: "9. Only pain is intellectual",
-      text: "9. With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.",
-    },
-  ],
-  alerts: [
-    {
-      type: "alert",
-      hover: "0. here is a lake",
-      text: "A lake is an area filled with water, localized in a basin, surrounded by land, and distinct from any river or other outlet that serves to feed or drain the lake",
-      alertText: "You're entering dangerous teritory",
-    },
-    {
-      type: "alert",
-      hover: "1. here is a lake",
-      text: "A lake is an area filled with water, localized in a basin, surrounded by land, and distinct from any river or other outlet that serves to feed or drain the lake",
-      alertText: "You're entering dangerous teritory",
-    },
-    {
-      type: "alert",
-      hover: "2. here is a lake",
-      text: "A lake is an area filled with water, localized in a basin, surrounded by land, and distinct from any river or other outlet that serves to feed or drain the lake",
-      alertText: "You're entering dangerous teritory",
-    },
-    {
-      type: "alert",
-      hover: "3. here is a lake",
-      text: "A lake is an area filled with water, localized in a basin, surrounded by land, and distinct from any river or other outlet that serves to feed or drain the lake",
-      alertText: "You're entering dangerous teritory",
-    },
-  ],
-  urls: [
-    {
-      type: "url",
-      hover: "0. For broader coverage of this topic, see Volcanism.",
-      text: "A volcano is a rupture in the crust of a planetary-mass object, such as Earth, that allows hot lava, volcanic ash, and gases to escape from a magma chamber below the surface.",
-      urlText: "whoops",
-      urlLink: "https://en.wikipedia.org/wiki/Volcanism",
-    },
-    {
-      type: "url",
-      hover: "1. For broader coverage of this topic, see Volcanism.",
-      text: "A volcano is a rupture in the crust of a planetary-mass object, such as Earth, that allows hot lava, volcanic ash, and gases to escape from a magma chamber below the surface.",
-      urlText: "whoops",
-      urlLink: "https://en.wikipedia.org/wiki/Volcanism",
-    },
-    {
-      type: "url",
-      hover: "2. For broader coverage of this topic, see Volcanism.",
-      text: "A volcano is a rupture in the crust of a planetary-mass object, such as Earth, that allows hot lava, volcanic ash, and gases to escape from a magma chamber below the surface.",
-      urlText: "whoops",
-      urlLink: "https://en.wikipedia.org/wiki/Volcanism",
-    },
-    {
-      type: "url",
-      hover: "3. For broader coverage of this topic, see Volcanism.",
-      text: "A volcano is a rupture in the crust of a planetary-mass object, such as Earth, that allows hot lava, volcanic ash, and gases to escape from a magma chamber below the surface.",
-      urlText: "whoops",
-      urlLink: "https://en.wikipedia.org/wiki/Volcanism",
-    },
-    {
-      type: "url",
-      hover: "4. For broader coverage of this topic, see Volcanism.",
-      text: "A volcano is a rupture in the crust of a planetary-mass object, such as Earth, that allows hot lava, volcanic ash, and gases to escape from a magma chamber below the surface.",
-      urlText: "whoops",
-      urlLink: "https://en.wikipedia.org/wiki/Volcanism",
-    },
-  ],
-};
-let tempCol = getComputedStyle(document.body);
-let colorArr = [
-  tempCol.getPropertyValue("--col1opt1"),
-  tempCol.getPropertyValue("--col1opt2"),
-  tempCol.getPropertyValue("--col2opt1"),
-  tempCol.getPropertyValue("--col2opt2"),
-  tempCol.getPropertyValue("--col2opt3"),
-];
-let placeArr = [];
-let positionArr = [];
-let hoverTextCount = 0;
-let correctClueNumber = -1;
-let gridSize = 2;
-const clueMax = 9; //the total number of places clicked
-
-for (i = 0; i < 68; i += gridSize) {
-  for (j = 0; j < 44; j += gridSize) {
+for (i = 1; i < 26; i++) {
+  photosArr.push("./assets/googleImgs/img" + i + ".png");
+}
+for (i = gridSize; i < 68; i += gridSize) {
+  for (j = gridSize; j < 44; j += gridSize) {
     positionArr.push([i, j]); //filling in all the posible values
   }
 }
@@ -219,11 +197,12 @@ function createGrid(i, j) {
   div.style.top = j + "vw"; //positioning div width
   grid.append(div); //adding to html page
 }
-function openWindow(urlTxt = "you won") {
-  // document.open("https://www.google.com", "_blank");
-  document.open();
-  document.write("<h1>" + urlTxt + "</h1>");
-  document.close();
+function openWindow(type, urlText = "oop") {
+  if (type == "win") {
+    window.location = "./ex1Win.html";
+  } else if (type == "link") {
+    window.location = "./ex1Link.html";
+  }
 }
 function progress() {
   progressWrapper.replaceChildren();
@@ -246,43 +225,59 @@ function doubleClickEvents(type, div, dblText = "null") {
     parseInt(div.id) == correctClueNumber + 1
       ? correctClueNumber++ && console.log("winning")
       : (correctClueNumber = -1);
-    correctClueNumber == clueMax ? openWindow() : console.log();
+    correctClueNumber == clueMax ? openWindow("win", "you won") : console.log();
     progress();
   } else if (type == "alerts") {
     alert(dblText); //displays alert
   } else if (type == "urls") {
-    openWindow(dblText); //fn call
+    openWindow("link", dblText); //fn call
+  } else if (type == "imgs") {
+    for (i = 0; i < Math.floor(Math.random() * 5); i++) {
+      let smallImg = document.createElement("img");
+      smallImg.src = photosArr[Math.floor(Math.random() * photosArr.length)];
+      smallImg.setAttribute("class", "imgsArr"); //set class w css properties
+      let position = Math.floor(Math.random() * (positionArr.length - 1));
+      smallImg.style.top = positionArr[position][0] + "vw"; //positioning div height
+      smallImg.style.left = positionArr[position][1] + "vw"; //positioning div width
+      smallImgsDiv.append(smallImg);
+      console.log(smallImg);
+    }
   }
 }
 class placeClass {
   constructor(type, index = 0) {
-    this.positon = Math.floor(Math.random() * (positionArr.length - 1)); //used in arrUpdate() this.positon = positionArr.length - 1; //for tests
-    this.width = positionArr[this.positon][0];
-    this.height = positionArr[this.positon][1];
+    this.position = Math.floor(Math.random() * (positionArr.length - 1)); //used in arrUpdate() this. position = positionArr.length - 1; //for tests
+    this.width = positionArr[this.position][0];
+    this.height = positionArr[this.position][1];
     this.placePosition = Math.floor(Math.random() * (placeOptions.length - 1)); //used in arrUpdate() choose a random place position to remove later
     this.placeName = placeOptions[this.placePosition]; //place name from array
+    this.hoverText = this.placeName; ////TO DO: comment to once hover text updated
     this.type = type; //adding the type to the constructor
     if (type == "hints") {
       this.clue = hoverTextCount; //exclusive property
-      this.hoverText = hoverTextJson.hints[this.clue].hover;
-      this.infoText = hoverTextJson.hints[this.clue].text;
+      //  this.hoverText = textJson.hints[this.clue].hover;//TO DO: uncomment to once hover text updated
+      this.infoText = textJson.hints[this.clue].text;
       this.color = colorArr[Math.floor(Math.random() * (colorArr.length - 1))]; //random col selector
     } else if (type == "alerts") {
-      this.hoverText = hoverTextJson.alerts[index].hover;
-      this.infoText = hoverTextJson.alerts[index].text;
-      this.alertText = hoverTextJson.alerts[index].alertText; //exclusive property
-      this.color = "#0000ff"; //hard coded blue
+      // this.hoverText = textJson.alerts[index].hover; //TO DO: uncomment to once hover text updated
+      this.infoText = textJson.alerts[index].text;
+      this.alertText = textJson.alerts[index].alertText; //exclusive property
+      this.color = colorArr[Math.floor(Math.random() * (colorArr.length - 1))]; //random col selector
     } else if (type == "urls") {
-      this.hoverText = hoverTextJson.urls[index].hover;
-      this.infoText = hoverTextJson.urls[index].text;
-      this.urlText = hoverTextJson.urls[index].urlText; //exclusive property
-      this.urlLink = hoverTextJson.urls[index].urlLink; //exclusive property
-      this.color = "#8E33FF"; //hard coded purple
+      // this.hoverText = textJson.urls[index].hover;//TO DO: uncomment to once hover text updated
+      this.infoText = textJson.urls[index].text;
+      this.urlText = textJson.urls[index].urlText; //exclusive property
+      this.urlLink = textJson.urls[index].urlLink; //exclusive property
+      this.color = colorArr[Math.floor(Math.random() * (colorArr.length - 1))]; //random col selector
+    } else if (type == "imgs") {
+      // this.hoverText = textJson.urls[index].hover;//TO DO: uncomment to once hover text updated
+      this.infoText = textJson.imgs[0].text;
+      this.color = colorArr[Math.floor(Math.random() * (colorArr.length - 1))]; //random col selector
     }
   }
   displayParameters() {
     console.log(
-      this.positon,
+      this.position,
       this.width,
       this.height,
       this.placePosition,
@@ -297,7 +292,7 @@ class placeClass {
     );
   }
   arrUpdate() {
-    positionArr.splice(this.positon, 1);
+    positionArr.splice(this.position, 1);
     placeOptions.splice(this.placePosition, 1);
   }
   //majority of fn take place below:
@@ -351,21 +346,27 @@ class placeClass {
     body.append(div); //adding to html page
   }
 }
-for (i = 0; i < hoverTextJson.hints.length; i++) {
+for (i = 0; i < textJson.hints.length; i++) {
   place = new placeClass("hints");
   place.arrUpdate();
   place.setDiv();
   place.setHoverDiv();
 }
-for (i = 0; i < hoverTextJson.alerts.length; i++) {
+for (i = 0; i < textJson.alerts.length; i++) {
   place = new placeClass("alerts", i);
   // place.displayParameters();
   place.arrUpdate();
   place.setDiv();
   place.setHoverDiv();
 }
-for (i = 0; i < hoverTextJson.urls.length; i++) {
+for (i = 0; i < textJson.urls.length; i++) {
   place = new placeClass("urls", i);
+  place.arrUpdate();
+  place.setDiv();
+  place.setHoverDiv();
+}
+for (i = 0; i < 6; i++) {
+  place = new placeClass("imgs", i);
   place.arrUpdate();
   place.setDiv();
   place.setHoverDiv();
