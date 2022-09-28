73de497ca28fef1db5898893ed72b4179a9ca4f7
TanviMishra <misht490@newschool.edu>
2022-09-28 04:58:21 -0400

    visual updates

--- projects/reponsiveSite/ex2-main.js
+++ projects/reponsiveSite/ex2-main.js
@@ -1,9 +1,18 @@
 let width, height, shape, layerUnit, layers, tempCol;
-let layerNum = 5;
-ctArr = ["#262626", "#333333", "#4d4d4d", "#666666", "#808080"];
-mntArr = ["#42210b", "#603813", "#754c24", "#8c6239", "#a67c52"];
-treeArr = ["#004924", "#006837", "#007c3e", "#009245", "#39b54a", "#7cb539"];
+let text = "A responsive site: resize the page to add some shappeees";
+let layerNum = 6;
+let bg = "#db9731";
+colArr = [
+  "#3f2735",
+  "#434ba0",
+  "#c03b31",
+  "#325f48",
+  "#e8a7ab",
+  "#efd289",
+  "#c03b31",
+];
 let content = document.querySelector("#content");
+let sky = document.querySelector("#sky");
 const startCount = 6;
 let colIndex = 0;
 let minW = 100 / startCount;
@@ -34,15 +43,12 @@ function checkRatio() {
   let state; //local variable
   if (height / width < 0.4) {
     // long view
-    $("body").css("background", "#c69c6d");
-    state = "mountain";
+    state = "trees";
   } else if (height / width < 0.8) {
     //horizontal view
-    $("body").css("background", "#8cc63f");
-    state = "trees";
+    state = "mountain";
   } else {
     //squarish
-    $("body").css("background", "#999999");
     state = "city";
   }
   return checkFnCall(state);
@@ -53,7 +59,7 @@ function layerCall(col) {
 function drawMountain(col) {
   triangle = document.createElement("div");
   triangle.setAttribute("class", "triangle");
-  triangle.style.backgroundColor = mntArr[colIndex];
+  triangle.style.backgroundColor = colArr[colIndex];
 }
 function drawTree(col) {
   col2 = col + 1;
@@ -63,24 +69,32 @@ function drawTree(col) {
   let height = "50%";
   leaves.style.width = radius;
   leaves.style.height = radius;
-  leaves.style.backgroundColor = treeArr[col2];
+  leaves.style.backgroundColor = colArr[col];
   trunk = document.createElement("div");
   trunk.style.width = "10px";
   trunk.style.height = height;
   trunk.setAttribute("class", "trunk");
-  trunk.style.backgroundColor = treeArr[col];
+  trunk.style.backgroundColor = colArr[col];
 }
 function drawCity() {
   city = document.createElement("div");
   city.setAttribute("class", "city");
-  city.style.backgroundColor = ctArr[colIndex];
+  city.style.backgroundColor = colArr[colIndex];
 }
 function shapeCheck(shapeContain) {
   if (shape == "mountain") {
+    minW = 100 / startCount;
+    maxW = 100 / (startCount - 4);
+    minH = 60;
+    maxH = 100;
     drawMountain(colIndex);
     shapeContain.append(triangle);
   }
   if (shape == "city") {
+    minW = 5;
+    maxW = 100 / startCount;
+    minH = 30;
+    maxH = 100;
     drawCity(colIndex);
     if (Math.random() > 0.7) {
       shapeContain.style.marginLeft = "5px";
@@ -88,6 +102,10 @@ function shapeCheck(shapeContain) {
     shapeContain.append(city);
   }
   if (shape == "trees") {
+    minW = 10;
+    maxW = 30;
+    minH = 70;
+    maxH = 100;
     drawTree(colIndex);
     shapeContain.append(leaves, trunk);
   }
@@ -103,9 +121,7 @@ function shapePos1(shapeContain) {
 function shapePos2(shapeContain) {
   shapeContain.style.position = "absolute";
   shapeContain.style.bottom = 0;
-  shapeContain.style.left = `${
-    Math.floor(Math.random() * (100 - 0 + 1)) + 0
-  }vw`;
+  shapeContain.style.left = `${Math.floor(Math.random() * (100 - 0 + 1)) + 0}%`;
   shapeContain.style.height = `${
     Math.floor(Math.random() * (maxH - minH + 1)) + minH
   }%`;
@@ -118,6 +134,7 @@ function drawShape(shapeContain) {
   shapeCheck(shapeContain);
 }
 function shapeChange() {
+  document.querySelector("p").innerHTML = text;
   layerCall();
   colIndex = 0;
   layers.forEach((l) => {
@@ -133,6 +150,8 @@ function shapeChange() {
 }
 function addShape() {
   if (Math.random() > 0.7) {
+    moveClouds();
+    document.querySelector("p").innerHTML += "s";
     let index = Math.floor(Math.random() * layers.length);
     l = layers[index];
     colIndex = index;
@@ -150,6 +169,27 @@ function createLayers() {
     layer.setAttribute("id", `layer${i}`);
     content.append(layer);
   }
+}
+function moveClouds() {
+  console.log("move them");
+  if (document.querySelectorAll(".cloud")) {
+    clouds = document.querySelectorAll(".cloud");
+    clouds.forEach((c) => {
+      if (parseInt(c.style.right, 10) < 65) {
+        c.style.right = parseInt(c.style.right, 10) + 1 + "vw";
+      } else {
+        c.style.right = "0vw";
+        c.style.top = `${Math.floor(Math.random() * (25 - 0 + 1)) + 0}vh`;
+      }
+    });
+  }
+}
+for (i = 0; i < 3; i++) {
+  let cloud = document.createElement("div");
+  cloud.setAttribute("class", "cloud");
+  cloud.style.top = `${Math.floor(Math.random() * (25 - 0 + 1)) + 0}vh`;
+  cloud.style.right = `${Math.floor(Math.random() * (60 - 10 + 1)) + 10}vw`;
+  sky.append(cloud);
 }
 //EVENTS
 //
